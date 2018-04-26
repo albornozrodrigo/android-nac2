5a54ea0ef5ac9e83e9e9589c3a6434b6ce8a103d
Rodrigo Albornoz <rodrigo.albornoz.f@gmail.com>
2018-04-26 02:25:05 -0300

    fucking database

---
+++ app/src/main/java/br/com/fiap/trabalho/rm79388/LogService.java
@@ -0,0 +1,31 @@
+package br.com.fiap.trabalho.rm79388;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class LogService extends Database {
+    public static final String TABLE = "log";
+
+    public LogService(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        super.onCreate(db);
+        String sql = "CREATE TABLE " + TABLE + " (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `description` TEXT, `date` INTEGER);";
+        db.execSQL(sql);
+    }
+
+    public void insert(String activity) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues cv = new ContentValues();
+        cv.put("description", activity);
+        cv.put("date", System.currentTimeMillis() / 1000);
+        db.insert(TABLE, null, cv);
+    }
+}
